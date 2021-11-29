1d39b6f123d4ce2c8086748beb5605ac2d593477
Caleb J. Ross <calebjross@gmail.com>
2021-11-29 14:57:09 -0600

    Intro scene started
    Ready for scene fade to first playable scene using IntroControl.cs

---
+++ Assets/IntroControl.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+
+public class IntroControl : MonoBehaviour
+{
+
+    #region Fields
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        
+    }
+}
+    #endregion
--- Assets/Scripts/Utilities/CameraMovement.cs
+++ Assets/Scripts/Utilities/CameraMovement.cs
@@ -34,28 +34,32 @@ public class CameraMovement : MonoBehaviour
     {
         smoothing = 0.1f;
 
-        if (target == null)
-        {
-            target = GameObject.FindGameObjectWithTag("Player").transform;
-        }
-
         //set the position of the camera to the player at the start of the scene
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
     }
 
     void LateUpdate()
     {
-        if (transform.position != target.position)
+        if (target != null)
         {
-            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
 
-            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
-            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
-
-            if (Vector3.Distance(targetPosition, transform.position) >= 0.05f)
+            if (transform.position != target.position)
             {
-                transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
-            } else transform.position = targetPosition;
+                Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+                targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+                targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+                if (Vector3.Distance(targetPosition, transform.position) >= 0.05f)
+                {
+                    transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+                }
+                else transform.position = targetPosition;
+            }
+        }
+        else
+        {
+            Debug.Log("No target set for main camera");
         }
     }
 }
